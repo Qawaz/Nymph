36f71896dded55d05008544804c13131aed2a5a5
Secret Keeper <shervin.ourfamily@gmail.com>
2022-01-08 20:35:47 +0330

    added redux-persist and react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,29 @@
 import { AppProps } from "next/app";
-import { AnimateSharedLayout } from "framer-motion";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "@/styles/global.css";
+import store from "@/redux/store";
+
+const persistor = persistStore(store);
+const queryClient = new QueryClient();
 
 export default function WhispersW3({ Component, pageProps }: AppProps) {
   return (
-    <div className="whispers-w3">
-      <AnimateSharedLayout>
-        <Component {...pageProps} />
-      </AnimateSharedLayout>
-      <style jsx>{`
-        .whispers-w3 {
-          background: #1f2228;
-        }
-      `}</style>
-    </div>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <div className="whispers-w3">
+            <Component {...pageProps} />
+            <style jsx>{`
+              .whispers-w3 {
+                background: #1f2228;
+              }
+            `}</style>
+          </div>
+        </QueryClientProvider>
+      </PersistGate>
+    </Provider>
   );
 }
