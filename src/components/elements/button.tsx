f1c6c35ba7b246a9e7d7c2159eb10ce0bbe361d4
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-18 21:29:09 +0430

    add type props and disabled style

--- src/components/elements/button.tsx
+++ src/components/elements/button.tsx
@@ -1,14 +1,16 @@
 import React, { FunctionComponent } from "react";
 
 type ButtonProps = {
+  type?: "button" | "submit" | "reset" | undefined;
   block?: boolean;
   disabled?: boolean;
-  onClick: () => void | Promise<any>;
+  onClick?: () => void | Promise<any>;
   tailwindColorClass?: string;
   tailwindTextColorClass?: string;
 };
 
 const Button: FunctionComponent<ButtonProps> = ({
+  type,
   children,
   onClick,
   disabled,
@@ -19,9 +21,12 @@ const Button: FunctionComponent<ButtonProps> = ({
 }) => (
   <>
     <button
+      type={type}
       className={`${
         block && "w-full"
-      } ${tailwindColorClass} ${tailwindTextColorClass} font-medium text-base text-white disabled:cursor-not-allowed rounded-md px-4 py-4`}
+      } ${tailwindColorClass} ${tailwindTextColorClass} ${
+        disabled && "opacity-50"
+      } font-medium text-base text-white disabled:cursor-not-allowed rounded-md px-4 py-4`}
       onClick={onClick}
       disabled={disabled}
       {...props}
