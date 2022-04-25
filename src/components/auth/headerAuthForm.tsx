94e6c48000a8b91500c9e5d704727367ff95b754
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-25 13:54:57 +0430

    init

---
+++ src/components/auth/headerAuthForm.tsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+const HeaderAuthForm = () => {
+  return (
+    <Fragment>
+      <h3 className="uppercase text-xl font-semibold mb-6 text-gray-400">
+        Stay for free
+      </h3>
+      <h1 className="font-semibold text-5xl text-white mb-8">
+        Create new account<span className="text-blue-500">.</span>
+      </h1>
+    </Fragment>
+  );
+};
+
+export default HeaderAuthForm;
