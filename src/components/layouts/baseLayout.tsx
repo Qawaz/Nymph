e93b9b86cd0dcc8985fffbbc5fff230ce1748b18
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-26 17:27:05 +0430

    removed page animation router

--- src/components/layouts/baseLayout.tsx
+++ src/components/layouts/baseLayout.tsx
@@ -1,27 +1,28 @@
 import { FunctionComponent } from "react";
 import Link from "next/link";
-import { AnimateSharedLayout, motion } from "framer-motion";
 
 const BaseLayout: FunctionComponent = ({ children }): JSX.Element => {
   return (
-    <div className="flex flex-col items-center w-full h-full py-16">
-      <div className="w-full max-w-screen-2xl">
-        <div className="flex flex-row items-start">
-          <motion.div className="font-medium text-white text-lg">
-            <Link href="/">Whispers</Link>
-          </motion.div>
-          <div className="flex flex-row ml-auto">
-            <div className="font-medium text-white text-lg mr-14">IBC</div>
-            <div className="font-medium text-white text-lg mr-14">
-              Build on Cosmos
+    <div className="container mx-auto">
+      <div className="py-16">
+        <div className="w-full max-w-screen-2xl">
+          <div className="flex flex-row items-start">
+            <div className="font-medium text-white text-lg">
+              <Link href="/">Whispers</Link>
             </div>
-            <div className="font-medium text-white text-lg mr-14">
-              Community
+            <div className="flex flex-row ml-auto">
+              <div className="font-medium text-white text-lg mr-14">IBC</div>
+              <div className="font-medium text-white text-lg mr-14">
+                Build on Cosmos
+              </div>
+              <div className="font-medium text-white text-lg mr-14">
+                Community
+              </div>
             </div>
           </div>
         </div>
+        {children}
       </div>
-      <AnimateSharedLayout>{children}</AnimateSharedLayout>
     </div>
   );
 };
