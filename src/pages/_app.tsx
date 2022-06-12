4da80bfd88102a5db48a7294692bdd1fd0934cf9
SecretKeeper <shervin.ourfamily@gmail.com>
2022-06-12 18:16:40 +0430

    add next-auth session provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,30 @@
+import { useState } from "react";
 import { AppProps } from "next/app";
+import store from "@/redux/store";
+import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import { QueryClient, QueryClientProvider } from "react-query";
+import RefreshTokenHandler from "@/components/refreshTokenHandler";
 import "@/styles/global.css";
-import store from "@/redux/store";
 
 const persistor = persistStore(store);
 const queryClient = new QueryClient();
 
-export default function WhispersW3({ Component, pageProps }: AppProps) {
+export default function Nymph({ Component, pageProps }: AppProps) {
+  const [interval, setInterval] = useState(0);
+
   return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      </PersistGate>
-    </Provider>
+    <SessionProvider session={pageProps.session} refetchInterval={interval}>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+          </QueryClientProvider>
+        </PersistGate>
+      </Provider>
+      <RefreshTokenHandler setInterval={setInterval} />
+    </SessionProvider>
   );
 }
