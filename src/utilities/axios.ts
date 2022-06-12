1a8229ff981c2cd2a168323639a0851e7c188f7b
SecretKeeper <shervin.ourfamily@gmail.com>
2022-06-12 12:36:08 +0430

    renamed utils folder to utilities

--- src/utilities/axios.ts
+++ src/utilities/axios.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const $axios = axios.create();
 
-$axios.defaults.baseURL = "http://127.0.0.1:3333";
+$axios.defaults.baseURL = "http://127.0.0.1:3335";
 
 export default $axios;
