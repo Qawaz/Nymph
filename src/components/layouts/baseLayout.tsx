f240bd5924cd9615ba5e6e917e47d445bad7b207
SecretKeeper <shervin.ourfamily@gmail.com>
2022-06-14 15:43:42 +0430

    code refactoring

--- src/components/layouts/baseLayout.tsx
+++ src/components/layouts/baseLayout.tsx
@@ -1,29 +1,23 @@
-import { FunctionComponent } from "react";
-import Link from "next/link";
+import { ReactNode } from "react";
 
-const BaseLayout: FunctionComponent = ({ children }): JSX.Element => {
+type Props = {
+  children: ReactNode;
+};
+
+const BaseLayout = ({ children }: Props): JSX.Element => {
   return (
-    <div className="container mx-auto">
-      <div className="py-16">
-        <div className="w-full max-w-screen-2xl">
-          <div className="flex flex-row items-start">
-            <div className="font-medium text-white text-lg">
-              <Link href="/">Whispers</Link>
-            </div>
-            <div className="flex flex-row ml-auto">
-              <div className="font-medium text-white text-lg mr-14">IBC</div>
-              <div className="font-medium text-white text-lg mr-14">
-                Build on Cosmos
-              </div>
-              <div className="font-medium text-white text-lg mr-14">
-                Community
-              </div>
-            </div>
-          </div>
-        </div>
-        {children}
-      </div>
-    </div>
+    <>
+      <main className="flex justify-center">
+        <div className="container">{children}</div>
+      </main>
+      <style jsx>
+        {`
+          main {
+            background-color: #131417;
+          }
+        `}
+      </style>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,26 @@
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
-import { useAppSelector, useAppDispatch } from "@/redux/appHooks";
 import IndexHeader from "@/components/index/header.index";
 import NoTrackingSection from "@/components/index/noTracking.index";
 import RegisterWithWalletSection from "@/components/index/registerWithWallet.index";
 import BlogsSection from "@/components/index/blogs.index";
+import BaseLayout from "@/components/layouts/baseLayout";
 
 const Index = () => {
   const router = useRouter();
   const { data: session, status } = useSession();
-
-  const count = useAppSelector((state) => state.counter.value);
-  const dispatch = useAppDispatch();
 
   if (status === "authenticated") {
     return <p>Signed in as {session.user.email}</p>;
   }
 
   return (
-    <>
-      <main>
-        <IndexHeader />
-        <NoTrackingSection />
-        <RegisterWithWalletSection />
-        <BlogsSection />
-      </main>
-      <style jsx>
-        {`
-          main {
-            background-color: rgb(19, 17, 32);
-          }
-        `}
-      </style>
-    </>
+    <BaseLayout>
+      <IndexHeader />
+      <NoTrackingSection />
+      <RegisterWithWalletSection />
+      <BlogsSection />
+    </BaseLayout>
   );
 };
 
