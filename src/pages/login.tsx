9f3a6b65f3af2ab9288ded2c33baede760a5b93a
SecretKeeper <secretk33per@icloud.com>
2022-07-04 11:48:35 +0430

    separate login header

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,11 @@
 import { SubmitHandler } from "react-hook-form";
 import BaseLayout from "@/components/layouts/baseLayout";
-import RegisterHeaderForm from "@/components/auth/register/header.register";
 import SocialSignIn from "@/components/auth/register/socialSignIn.register";
 import LoginForm from "@/components/auth/login/form.login";
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 import { useRouter } from "next/router";
+import LoginHeaderForm from "@/components/auth/login/header.login";
 
 export type Inputs = {
   username_or_email: string;
@@ -31,7 +31,7 @@ const Register = () => {
 
   return (
     <BaseLayout>
-      <RegisterHeaderForm />
+      <LoginHeaderForm />
       <div className="grid grid-cols-1 md:grid-cols-2 bg-dark-blur rounded-md mb-24">
         <div className="w-full border-r border-r-gray-800 px-32">
           <LoginForm onSubmit={onSubmit} error={error} />
