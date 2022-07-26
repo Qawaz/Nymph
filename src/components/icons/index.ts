8d8a6e834a15272732feece0c7a4baa74f4afc21
SecretKeeper <secretk33per@icloud.com>
2022-07-27 01:01:09 +0430

    added new icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,6 +1,17 @@
 import AppleIcon from "./apple";
+import ContactsIcon from "./contacts";
 import GoogleIcon from "./google";
 import MediumIcon from "./medium";
+import NewMessageIcon from "./newMessage";
+import SettingsIcon from "./settings";
 import Unauthorized from "./unathorized";
 
-export { AppleIcon, GoogleIcon, MediumIcon, Unauthorized };
+export {
+  AppleIcon,
+  ContactsIcon,
+  GoogleIcon,
+  MediumIcon,
+  NewMessageIcon,
+  SettingsIcon,
+  Unauthorized,
+};
