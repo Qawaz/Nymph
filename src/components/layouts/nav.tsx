56676048c3f4b8ae3a08f11279ea63279702ddff
SecretKeeper <secretk33per@icloud.com>
2022-07-27 01:08:20 +0430

    init

---
+++ src/components/layouts/nav.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { SettingsIcon } from "../icons";
+
+const Nav = () => {
+  return (
+    <div className="flex flex-col text-white w-20 bg-gradient-to-b from-[#3b3132] to-[#271b25]">
+      <div className="flex-grow">Whisper</div>
+      <div className="flex flex-col justify-center items-center pb-5">
+        <div className="hover:cursor-pointer mb-5">
+          <SettingsIcon />
+        </div>
+        <Image
+          src="https://avatarfiles.alphacoders.com/217/217821.png"
+          alt="Avatar"
+          width={40}
+          height={40}
+          className="rounded-full hover:cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
