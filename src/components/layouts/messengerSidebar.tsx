3214666a0da02b2a019910de6408e0028944db20
SecretKeeper <secretk33per@icloud.com>
2022-07-27 01:08:24 +0430

    init

---
+++ src/components/layouts/messengerSidebar.tsx
@@ -0,0 +1,85 @@
+import MessageSidebarSearch from "@/components/messenger/messengerSidebar/search";
+import { ContactsIcon, NewMessageIcon } from "@/components/icons";
+import Channels from "@/components/containers/channels";
+import Conversations from "@/components/containers/conversations";
+import Shortcut from "@/components/messenger/messengerSidebar/shortcut";
+
+const shortcuts = [
+  {
+    title: "Contacts",
+    icon: <ContactsIcon />,
+  },
+  {
+    title: "New Message",
+    icon: <NewMessageIcon />,
+  },
+  {
+    title: "Settings",
+    icon: <ContactsIcon />,
+  },
+];
+
+const MessengerSidebar = () => {
+  return (
+    <div className="text-white w-[400px] overflow-y-scroll max-h-screen bg-gradient-to-b from-[#3b3132] to-[#271b25] py-4">
+      <MessageSidebarSearch />
+      <div className="px-4 mt-2 ">
+        {shortcuts.map((shortcut, index) => (
+          <Shortcut key={index} {...shortcut} />
+        ))}
+      </div>
+      <Channels />
+      <Conversations />
+      {/* <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div>
+      <div>i am messenger sidebar</div> */}
+    </div>
+  );
+};
+
+export default MessengerSidebar;
