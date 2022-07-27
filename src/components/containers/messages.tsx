b960f2764e8beb2f41cb0e30414408c2138e6b61
SecretKeeper <secretk33per@icloud.com>
2022-07-27 23:46:10 +0430

    added MessagesContainer component

--- src/components/containers/messages.tsx
+++ src/components/containers/messages.tsx
@@ -1,11 +1,12 @@
 import MessagesHeader from "@/components/messages/messagesHeader";
 import MessagesFooter from "@/components/messages/messagesFooter";
+import MessagesContainer from "@/components/messages/messagesContainer";
 
 const Messages = () => {
   return (
     <div className="flex flex-col flex-grow max-h-screen bg-gradient-to-b from-[#271c1c] to-[#27161e]">
       <MessagesHeader />
-      <div className="flex-grow">Messagses Container</div>
+      <MessagesContainer />
       <MessagesFooter />
     </div>
   );
