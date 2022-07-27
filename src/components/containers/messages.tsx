2984c59fc4cdde390d8c14df6d65e881cd81f4a6
SecretKeeper <secretk33per@icloud.com>
2022-07-27 16:38:28 +0430

    added messages footer

--- src/components/containers/messages.tsx
+++ src/components/containers/messages.tsx
@@ -1,10 +1,12 @@
 import MessagesHeader from "@/components/messages/messagesHeader";
+import MessagesFooter from "@/components/messages/messagesFooter";
 
 const Messages = () => {
   return (
-    <div className="flex-grow max-h-screen bg-gradient-to-b from-[#271c1c] to-[#27161e]">
+    <div className="flex flex-col flex-grow max-h-screen bg-gradient-to-b from-[#271c1c] to-[#27161e]">
       <MessagesHeader />
-      Messagses Container
+      <div className="flex-grow">Messagses Container</div>
+      <MessagesFooter />
     </div>
   );
 };
---
+++ src/components/messages/messagesFooter.tsx
@@ -0,0 +1,36 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { AttachIcon, SendIcon } from "../icons";
+
+type Input = {
+  message: string;
+};
+
+const MessagesFooter = (): JSX.Element => {
+  const { register, handleSubmit } = useForm<Input>();
+
+  const onSubmit: SubmitHandler<Input> = (data) => console.log(data);
+  return (
+    <div className="flex items-center px-8 mb-4">
+      <form
+        className="flex flex-grow items-center rounded bg-[#fff]/10 pr-4"
+        onSubmit={handleSubmit(onSubmit)}
+        autoComplete="off"
+      >
+        <input
+          type="text"
+          className="w-full bg-transparent text-sm rounded text-gray-200 placeholder:text-gray-500 py-3 px-4"
+          placeholder="Type something..."
+          {...register("message")}
+        />
+        <button className="border-r mr-3 pr-3 ">
+          <AttachIcon />
+        </button>
+        <button type="submit">
+          <SendIcon />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default MessagesFooter;
