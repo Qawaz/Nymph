5085152889edae50fbeb6d371096eff89395404a
SecretKeeper <secretk33per@icloud.com>
2022-07-27 15:21:14 +0430

    init

---
+++ src/components/containers/messages.tsx
@@ -0,0 +1,12 @@
+import MessagesHeader from "@/components/messages/messagesHeader";
+
+const Messages = () => {
+  return (
+    <div className="flex-grow max-h-screen bg-gradient-to-b from-[#271c1c] to-[#27161e]">
+      <MessagesHeader />
+      Messagses Container
+    </div>
+  );
+};
+
+export default Messages;
