6865a583bb9765a9c90df60b24d003fb76debd2c
SecretKeeper <secretk33per@icloud.com>
2022-07-27 16:39:43 +0430

    added attach and send icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,17 +1,21 @@
 import AppleIcon from "./apple";
+import AttachIcon from "./attach";
 import ContactsIcon from "./contacts";
 import GoogleIcon from "./google";
 import MediumIcon from "./medium";
 import NewMessageIcon from "./newMessage";
 import SettingsIcon from "./settings";
+import SendIcon from "./send";
 import Unauthorized from "./unathorized";
 
 export {
   AppleIcon,
+  AttachIcon,
   ContactsIcon,
   GoogleIcon,
   MediumIcon,
   NewMessageIcon,
   SettingsIcon,
+  SendIcon,
   Unauthorized,
 };
