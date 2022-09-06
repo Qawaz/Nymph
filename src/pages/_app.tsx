ce815fb18fb87f69d7ff3ae11b6010c79da36340
Hassan Haghniya <secretk33p3r@gmail.com>
2022-09-06 13:30:16 +0430

    enable react query devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import RefreshTokenHandler from "@/components/refreshTokenHandler";
 import "@/styles/global.css";
 
@@ -14,6 +15,7 @@ export default function Nymph({ Component, pageProps }: AppProps) {
     <SessionProvider session={pageProps.session} refetchInterval={interval}>
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
       <RefreshTokenHandler setInterval={setInterval} />
     </SessionProvider>
