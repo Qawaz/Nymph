4e96da267be8459c6c6fa06149ad414c43af0fc6
SecretKeeper <TheSecretK33per@gmail.com>
2023-04-12 22:35:44 +0330

    redesinged login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,18 @@
 import { SubmitHandler } from "react-hook-form";
-import BaseLayout from "@/components/layouts/baseLayout";
-import SocialSignIn from "@/components/auth/register/socialSignIn.register";
 import LoginForm from "@/components/auth/login/form.login";
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import LoginHeaderForm from "@/components/auth/login/header.login";
+import AuthLayout from "@/components/layouts/authLayout";
+import Link from "next/link";
 
 export type Inputs = {
   username_or_email: string;
   password: string;
 };
 
-const Register = () => {
+const Register = (): JSX.Element => {
   const router = useRouter();
 
   const [error, setError] = useState("");
@@ -30,26 +30,17 @@ const Register = () => {
     });
 
   return (
-    <BaseLayout>
-      <LoginHeaderForm />
-      <div className="grid grid-cols-1 md:grid-cols-2 bg-dark-blur rounded-md mb-24">
-        <div className="w-full border-r border-r-gray-800 px-32">
+    <AuthLayout>
+      <div className="flex flex-col h-full py-24 px-20">
+        <div>
+          <LoginHeaderForm />
           <LoginForm onSubmit={onSubmit} error={error} />
         </div>
-        <div className="px-32">
-          <SocialSignIn />
+        <div className="flex flex-grow justify-center items-end text-sm">
+          <Link href="/register">Don't have an account? Register here</Link>
         </div>
       </div>
-      <div className="text-gray-300 text-center text-base pb-16">
-        Forgot Password?
-      </div>
-      <div className="flex py-10">
-        <div className="text-gray-400 text-center">Privacy Policy</div>
-        <div className="text-gray-400 text-center ml-auto">
-          Copyright @ Nymph 2022
-        </div>
-      </div>
-    </BaseLayout>
+    </AuthLayout>
   );
 };
 
