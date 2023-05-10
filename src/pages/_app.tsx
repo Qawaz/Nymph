a804fe623a454b50bf1d234a84fd4b6f6f336c93
SecretKeeper <TheSecretK33per@gmail.com>
2023-05-10 12:09:41 +0330

    removed react-query implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,15 @@
 import { useState } from "react";
 import { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import RefreshTokenHandler from "@/components/refreshTokenHandler";
 import "@/styles/global.css";
-
-const queryClient = new QueryClient();
 
 export default function Nymph({ Component, pageProps }: AppProps) {
   const [interval, setInterval] = useState(0);
 
   return (
     <SessionProvider session={pageProps.session} refetchInterval={interval}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <Component {...pageProps} />
       <RefreshTokenHandler setInterval={setInterval} />
     </SessionProvider>
   );
