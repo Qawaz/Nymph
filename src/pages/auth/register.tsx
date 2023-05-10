55b2a16e2d167c373415ad67c5d991b72efd5d2f
SecretKeeper <TheSecretK33per@gmail.com>
2023-05-10 13:12:44 +0330

    moved register page to /auth

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,16 +1,7 @@
-import { SubmitHandler } from "react-hook-form";
 import Link from "next/link";
-import { useState } from "react";
-import { useRouter } from "next/router";
 import AuthLayout from "@/components/layouts/authLayout";
 import RegisterHeaderForm from "@/components/auth/register/header.register";
 import RegisterForm from "@/components/auth/register/form.register";
-
-export type Inputs = {
-  username: string;
-  email: string;
-  password: string;
-};
 
 const Register = (): JSX.Element => (
   <AuthLayout>
