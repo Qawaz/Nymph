b120e1a1687d5b4e9aa7025614136a4ffc2054f2
SecretKeeper <TheSecretK33per@gmail.com>
2023-05-12 00:42:32 +0330

    set destination props for linking

--- src/components/nav/items.nav.tsx
+++ src/components/nav/items.nav.tsx
@@ -1,11 +1,25 @@
-const NavItem = ({ title, icon }) => (
-  <button
-    type="button"
-    className="relative inline-flex items-center w-full px-4 py-4 text-gray-800 text-sm font-medium hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
-  >
-    {icon}
-    {title}
-  </button>
-);
+import { useRouter } from "next/navigation";
+import React from "react";
+
+type Props = {
+  title: string;
+  icon: React.ReactNode;
+  destination: string;
+};
+
+const NavItem = ({ title, icon, destination }: Props) => {
+  const router = useRouter();
+
+  return (
+    <button
+      type="button"
+      className="relative inline-flex items-center w-full px-4 py-4 text-gray-800 text-sm font-medium hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
+      onClick={() => router.push(destination)}
+    >
+      {icon}
+      {title}
+    </button>
+  );
+};
 
 export default NavItem;
