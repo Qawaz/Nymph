b4f872bc237a5d1fac8b105900df91a8b02f01b4
SecretKeeper <secretk33p3r@gmail.com>
2023-05-13 17:16:54 +0430

    change some style

--- src/components/messenger/messengerSidebar/conversation.tsx
+++ src/components/messenger/messengerSidebar/conversation.tsx
@@ -23,7 +23,7 @@ const Conversation = ({
   return (
     <div
       onClick={() => setConversation({ id, username, avatar_url })}
-      className="flex flex-grow items-center hover:cursor-pointer hover:bg-[#f5f7f9] px-4 py-4"
+      className="flex flex-grow items-center overflow-hidden hover:cursor-pointer hover:bg-[#f5f7f9] px-4 py-4"
     >
       <figure className="relative w-11 h-11 mr-1">
         <Image
@@ -33,10 +33,12 @@ const Conversation = ({
           fill={true}
         />
       </figure>
-      <div className="pl-2 flex-grow">
+      <div className="pl-2 flex-grow overflow-hidden">
         <div className="text-sm text-gray-700 mb-1">{username}</div>
         <div className="flex">
-          <div className="text-xs flex-grow text-gray-400">{last_message}</div>
+          <div className="truncate overflow-hidden text-xs flex-grow text-gray-400">
+            {last_message}
+          </div>
           <div className="text-xs text-gray-500">{last_time_message}</div>
         </div>
       </div>
